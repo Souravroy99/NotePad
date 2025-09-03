b3c5e8834e1f00d20c98f04df43f8c982ed8df94
Sourav Roy <122677511+Souravroy99@users.noreply.github.com>
2025-09-04 00:29:26 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: "https://notepad-backend-hehh.onrender.com/api"
 });
 
 export default api

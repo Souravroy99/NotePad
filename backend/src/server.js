1167703deb9c1cbff7a05d7fedd4bb3c2c1a5151
Souravroy99 <souravroy200418@gmail.com>
2025-09-03 21:29:10 +0530

    Add into github

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from "cors"
+
+import notesRouter from './routes/notesRoutes.js'
+import { connectDB } from './config/db.js'
+import rateLimiter from './middleware/rateLimiter.js'
+
+
+const app = express()
+const port = process.env?.PORT || 6001
+
+// Middleware
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}))
+app.use(express.json())
+app.use(rateLimiter)
+
+
+app.use("/api/notes", notesRouter)
+
+
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Server listening on port no: ${port}`);
+    })
+})
+.catch(() => {
+    console.error("Failed to connect to the database:", err);
+    process.exit(1);
+})
